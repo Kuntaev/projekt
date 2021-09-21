48673b9ee76b495bbd722a5a9604ea0fc25de2ba
Khatciev <killermen095@mail.ru>
2021-09-21 09:21:55 +0300

    Фиксация

--- routes/captain.route.js
+++ routes/captain.route.js
@@ -1,2 +1,10 @@
 const {Router} = require("express")
 const {captainController} = require("../controllers/caption.controller");
+
+const router = Router()
+
+router.post("/registration", captainController.registrationCaption)
+router.post("/authorization", captainController.authorizationCaptain)
+router.get("/captain", captainController.getCaptions)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const { Router } = require("express")
 
 const router = Router()
 
+router.use(require("./captain.route"))
+
 module.exports = router
