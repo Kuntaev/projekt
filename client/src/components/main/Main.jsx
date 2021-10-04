42556200057f8091251d202022f28a325551c9f4
Kuntaev <maga5252744@mail.ru>
2021-10-04 22:39:14 +0300

    nt

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Header from '../header/Header';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "../header/Header";
+import image from "../../image/e7c6e2df-765f-4cd1-b17c-e7aabc8ed0cf.png";
 
 const useStyles = makeStyles({
   main: {
-    backgroundSize:'cover',
-    backgroundImage: `url("https://i.postimg.cc/Y2grKQDP/xxx.jpg")`,
-    background: 'no-repeat',
-    minHeight:'100vh'
+    backgroundSize: "cover",
+    backgroundImage: `URL(${image})`,
+    background: "no-repeat",
+    minHeight: "100vh",
+    backgroundColor: "#454d35",
   },
-  content:{
-    paddingLeft:150,
-    paddingRight:150,
-    marginTop:150
+  content: {
+    paddingLeft: 150,
+    paddingRight: 150,
+    marginTop: 150,
   },
-  text:{
-    color:'white',
-    fontFamily:'Roboto',
-    fontWeight: 'bold'
-  }
-
-})
+  text: {
+    color: "white",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+  },
+  btn: {
+    fontSize: 25,
+    padding: 15,
+    marginLeft: 1000,
+  },
+});
 
 function Main() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className={classes.main}>
       <Header />
       <div className={classes.content}>
-        <h4 className={classes.text} style={{fontSize:30, marginBottom:10}}>В ФУТБОЛЕ НЕТ СЛОВА</h4>
-        <h2 className={classes.text} style={{fontSize:64, marginTop:0}}>НЕВОЗМОЖНО</h2>
+        <h4 className={classes.text} style={{ fontSize: 30, marginBottom: 10 }}>
+          В ФУТБОЛЕ НЕТ СЛОВА
+        </h4>
+        <h2 className={classes.text} style={{ fontSize: 64, marginTop: 0 }}>
+          НЕВОЗМОЖНО
+        </h2>
       </div>
     </div>
   );
