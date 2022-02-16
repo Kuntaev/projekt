b9a643d6a9545b4e84577d9f0536890d0f0f154e
Abdullah Mamakaev <abdurrohim9505@gmail.com>
2022-02-16 23:37:05 +0300

    Пофиксил баг, теперь поля обязательно нужно заполнить

--- client/src/components/myTeam/CreateTeam.jsx
+++ client/src/components/myTeam/CreateTeam.jsx
@@ -15,13 +15,23 @@ function CreateTeam({ state, onClose }) {
 
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [imageError, setImageError] = useState(false);
   const [open, setOpen] = React.useState(false);
 
   const handleAddTeam = () => {
-    setOpen(false);
-    dispatch(addTeam(name, image));
-    setName("");
-    setImage("");
+    if (name == "") {
+      setNameError(true);
+    }
+    if (image == "") {
+      setImageError(true);
+    }
+    if (image && name) {
+      setOpen(false);
+      dispatch(addTeam(name, image));
+      setName("");
+      setImage("");
+    }
   };
   const handleAddName = (e) => {
     setName(e.target.value);
@@ -34,30 +44,40 @@ function CreateTeam({ state, onClose }) {
     <Dialog open={state} onClose={onClose}>
       <DialogTitle>Заполните поля, чтобы создать команду...</DialogTitle>
       <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Название команды"
-          type="name"
-          onChange={handleAddName}
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Ссылка логотипа"
-          id="name"
-          type="url"
-          fullWidth
-          variant="standard"
-          onChange={handleAddImage}
-        />
+        <form noValidate autoComplete="off" onSubmit={handleAddTeam}>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Название команды"
+            type="name"
+            onChange={handleAddName}
+            fullWidth
+            variant="outlined"
+            required
+            error={nameError}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Ссылка логотипа"
+            id="name"
+            type="url"
+            fullWidth
+            variant="outlined"
+            onChange={handleAddImage}
+            required
+            error={imageError}
+          />
+        </form>
       </DialogContent>
       <DialogActions>
-        <Button onClick={onClose}>Закрыть</Button>
-        <Button onClick={handleAddTeam}>Подтвердить</Button>
+        <Button onClick={onClose} type="submit" variant="contained">
+          Закрыть
+        </Button>
+        <Button onClick={handleAddTeam} type="submit" variant="contained">
+          Подтвердить
+        </Button>
       </DialogActions>
     </Dialog>
   );
--- client/src/components/play/AddPlay.jsx
+++ client/src/components/play/AddPlay.jsx
@@ -11,7 +11,6 @@ import { useDispatch } from "react-redux";
 import { addEvents } from "../../redux/features/event";
 
 export default function FormDialog({ state, onClose }) {
-
   const dispatch = useDispatch();
 
   const [width, setWidth] = useState("");
@@ -19,6 +18,11 @@ export default function FormDialog({ state, onClose }) {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [name, setName] = useState("");
+  const [widthError, setWidthError] = useState(false);
+  const [longsError, setLongsError] = useState(false);
+  const [dateError, setDateError] = useState(false);
+  const [timeError, setTimeError] = useState(false);
+  const [nameError, setNameError] = useState(false);
 
   const handleChangedName = (e) => {
     setName(e.target.value);
@@ -35,66 +39,101 @@ export default function FormDialog({ state, onClose }) {
   const handleChangedWidth = (e) => {
     setWidth(e.target.value);
   };
+
   const EvAdd = () => {
-    dispatch(addEvents(name, width, time, date, longs));
+    if (name == "") {
+      setNameError(true);
+    }
+    if (time == "") {
+      setTimeError(true);
+    }
+    if (date == "") {
+      setDateError(true);
+    }
+    if (longs == "" && longs < -90 && longs > 90) {
+      setLongsError(true);
+    }
+    if (width == "" && width < -90 && width > 90) {
+      setWidthError(true);
+    }
+    if (name && width && time && date && longs) {
+      dispatch(addEvents(name, width, time, date, longs));
+    }
   };
+
   return (
     <div>
       <Dialog open={state} onClose={onClose}>
         <DialogTitle>Заполните поля, чтобы назначить матч...</DialogTitle>
         <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Название"
-            type="name"
-            onChange={handleChangedName}
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            type="date"
-            fullWidth
-            variant="standard"
-            onChange={handleChangedDate}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            type="time"
-            fullWidth
-            variant="standard"
-            onChange={handleChangedTime}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="долгота"
-            type="long"
-            onChange={handleChangedLong}
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="ширина"
-            type="width"
-            onChange={handleChangedWidth}
-            fullWidth
-            variant="standard"
-          />
+          <form noValidate autoComplete="off" onSubmit={EvAdd}>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Название"
+              type="name"
+              onChange={handleChangedName}
+              fullWidth
+              variant="outlined"
+              required
+              error={nameError}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="date"
+              type="date"
+              fullWidt
+              onChange={handleChangedDate}
+              variant="outlined"
+              required
+              error={dateError}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="time"
+              type="time"
+              fullWidth
+              onChange={handleChangedTime}
+              variant="outlined"
+              required
+              error={timeError}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="longs"
+              label="долгота"
+              type="long"
+              onChange={handleChangedLong}
+              fullWidth
+              variant="outlined"
+              required
+              error={longsError}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="width"
+              label="ширина"
+              type="width"
+              onChange={handleChangedWidth}
+              fullWidth
+              variant="outlined"
+              required
+              error={widthError}
+            />
+          </form>
         </DialogContent>
         <DialogActions>
-          <Button onClick={onClose}>Закрыть</Button>
-          <Button onClick={EvAdd}>Подтвердить</Button>
+          <Button onClick={onClose} type="submit" variant="contained">
+            Закрыть
+          </Button>
+          <Button onClick={EvAdd} type="submit" variant="contained">
+            Подтвердить
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
